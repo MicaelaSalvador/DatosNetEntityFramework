3d7f3f2762ea3fe9d3a936fda6d9bb1e8fc53337
MicaelaSalvvador <sacmi9@hotmail.com>
2024-01-24 16:50:49 -0600

    addPrubas

---
+++ StudentEntityFramework/SOLOPRUEBA/Prueba.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentEntityFramework.SOLOPRUEBA
+{
+    public class Prueba
+    {
+
+        public int valor1 { get; set; }
+        public int valor2 { get; set; }
+        public int valor3 { get; set; }
+
+    }
+}
